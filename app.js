d51f683388601663d7398641b0b847aef6c23724
Ankit Raj <ankittrajj1996@gmail.com>
2022-02-13 07:31:21 -0600

    updated with add experience button

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // #adding new work experience field dynamically!!
 addNewExField = ()=>{
-    console.log('hello experience')
+    let newNode = document.createElement('textarea')
+    console.log(newNode)
+    newNode.classList.add('form-control')
+    newNode.classList.add('wefield')
+    newNode.classList.add('mt-2')
+    newNode.setAttribute('rows',3)
+    newNode.setAttribute('placeholder','Enter the qualification')
+
+    let addbtnobj = document.getElementById('workExpAddButton')
+    let workexpobj = document.getElementById('workExperience')
+    workexpobj.insertBefore(newNode,addbtnobj)
+
+
 }
